9d5ae32309ded37f72563b0d38cf5e4c1b20200d
Rubens Fernando Maciel Alencar <rubensfernandom@gmail.com>
2023-11-28 20:56:51 -0300

    Refactor page layout and styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,28 +4,92 @@
 
 <div class="container">
 	<Carousel />
-	<header>
-		<h1>Por dentro da recuperação do acervo de fotografias de Bob Wolfenson</h1>
-		<p>
-			Entenda como boa parte do acervo de um dos maiores fotógrafos brasileiros foi recuperada
-			depois de uma enchente que atingiu seu estúdio em fevereiro de 2020. E conheça os principais
-			responsáveis por dar uma vida nova a esses materiais fotográficos
-		</p>
-	</header>
-	<main>
-		<div class="card">
-			<div class="image">
-				<img src="https://picsum.photos/seed/1/195/144" alt="Foto" />
-			</div>
-			<div class="body">
-				<h3 class="title">dsdsds</h3>
-				<p class="subtitle">Osassasas</p>
+	<div class="content">
+		<div class="content__main">
+			<header class="header">
+				<h1>Por dentro da recuperação do acervo de fotografias de Bob Wolfenson</h1>
+				<p class="subtitle">
+					Entenda como boa parte do acervo de um dos maiores fotógrafos brasileiros foi recuperada
+					depois de uma enchente que atingiu seu estúdio em fevereiro de 2020. E conheça os
+					principais responsáveis por dar uma vida nova a esses materiais fotográficos
+				</p>
+			</header>
+			<div>
+				<div class="card">
+					<div class="image">
+						<img src="https://picsum.photos/seed/1/195/144" alt="Foto" />
+					</div>
+					<div class="body">
+						<h3 class="title">o processo</h3>
+						<p class="subtitle">Os principais momentos, da enchente à recuperação</p>
+					</div>
+				</div>
+
+				<div class="card">
+					<div class="image">
+						<img src="https://picsum.photos/seed/1/195/144" alt="Foto" />
+					</div>
+					<div class="body">
+						<h3 class="title">passo-a-passo</h3>
+						<p class="subtitle">O trabalho da equipe de recuperação do acervo</p>
+					</div>
+				</div>
+
+				<div class="card">
+					<div class="image">
+						<img src="https://picsum.photos/seed/1/195/144" alt="Foto" />
+					</div>
+					<div class="body">
+						<h3 class="title">quem é quem</h3>
+						<p class="subtitle">Saiba mais sobre os principais nomes envolvidos</p>
+					</div>
+				</div>
+
+				<div class="card">
+					<div class="image">
+						<img src="https://picsum.photos/seed/1/195/144" alt="Foto" />
+					</div>
+					<div class="body">
+						<h3 class="title">A recuperação</h3>
+						<p class="subtitle">Um acervo que, enfim, volta à vida</p>
+					</div>
+				</div>
+
+				<div class="card">
+					<div class="image">
+						<img src="https://picsum.photos/seed/1/195/144" alt="Foto" />
+					</div>
+					<div class="body">
+						<h3 class="title">mini-doc</h3>
+						<p class="subtitle">Lorem ipsum dolor sit amet consequeter lorem ipsum</p>
+					</div>
+				</div>
+
+				<div class="card">
+					<div class="image">
+						<img src="https://picsum.photos/seed/1/195/144" alt="Foto" />
+					</div>
+					<div class="body">
+						<h3 class="title">fotografias</h3>
+						<p class="subtitle">Uma seleção das imagens do acervo que foram recuperadas</p>
+					</div>
+				</div>
 			</div>
 		</div>
-	</main>
+	</div>
 </div>
 
 <style lang="scss">
+	h1,
+	.subtitle {
+		text-align: center;
+	}
+	h1 {
+		font-weight: 700;
+	}
+	.header {
+		margin: 2em 0;
+	}
 	.card {
 		display: flex;
 		flex-direction: column;
@@ -35,10 +99,20 @@
 			font-weight: 700;
 			text-transform: uppercase;
 			text-align: center;
-			margin-bottom: 12px;
 		}
 		.subtitle {
 			font-size: 11px;
+		}
+		& + .card {
+			margin-top: 24px;
+		}
+	}
+
+	.content {
+		display: flex;
+		justify-content: center;
+		&__main {
+			max-width: 768px;
 		}
 	}
 </style>
