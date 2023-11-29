7c139abc3a6358addf26a579116ea1fb5c148c6d
Rubens Fernando Maciel Alencar <rubensfernandom@gmail.com>
2023-11-29 00:10:45 -0300

    Add Image component

---
+++ src/lib/components/Image.svelte
@@ -0,0 +1,36 @@
+<script>
+	export let image = '';
+	export let caption = '';
+	export let credit = '';
+</script>
+
+<figure>
+	<img src={image} alt={caption} />
+	<figcaption>
+		{#if caption}
+			{caption}
+		{/if}
+		{#if credit}
+			<span class="credit">{credit}</span>
+		{/if}
+	</figcaption>
+</figure>
+
+<style lang="scss">
+	figure {
+		margin: 2em 0;
+		padding: 0;
+	}
+	figure > img {
+		width: 100%;
+	}
+	figure > figcaption {
+		font-size: 0.8rem;
+		font-weight: bold;
+		.credit {
+			font-size: 0.7rem;
+			font-weight: 400;
+			margin-left: 1em;
+		}
+	}
+</style>
