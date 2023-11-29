e3c09fdd11fc7c7480cfb8e1df454b1673bf603a
Rubens Fernando Maciel Alencar <rubensfernandom@gmail.com>
2023-11-29 00:30:53 -0300

    Add mdsvex dependency and configuration

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,26 @@
 import adapter from '@sveltejs/adapter-static';
 import sveltePreprocess from 'svelte-preprocess';
+import { mdsvex } from 'mdsvex'
+
+/** @type {import('mdsvex').MdsvexOptions} */
+const mdsvexOptions = {
+	extensions: ['.svx'],
+}
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: sveltePreprocess({
+	extensions: ['.svelte', '.md', '.mdx', '.svx'],
+	preprocess: [sveltePreprocess({
 		scss: {
 			prependData: `@import 'src/style/variables.scss';`
 		}
-	}),
+	}), mdsvex(mdsvexOptions)],
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+
 	}
 };
 
